79741b12a525a17f25f7dccceb5aa818ece87c1b
LazyBoyM <143833539+LazyBoyM@users.noreply.github.com>
2024-03-07 22:50:16 +0700

    fxh
    dxghrut

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Chị đẹp';
+const giftUrl = 'https://www.facebook.com/photo.php?fbid=1163030658410976&set=pb.100041121584832.-2207520000&type=3';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Gửi Chị';
+const contentCard = 'Chúc honey của em 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+const giftImage = 'nhu.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
